8b1318ac257af526ecc187970d3c80e61e3ca4e6
Manish Purohit <manishkumar9866@gmail.com>
2020-10-25 19:20:13 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,73 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
-    left: "0px",
-    top: "0px",
+  const [ballPosition, setBallPosition] = useState({
+    left: "0",
+    top: "0"
+  });
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.addEventListener("keydown",()=>handleKeyDown);  
+  });
+  const handleKeyDown = (event) => {
+    switch (event.keyCode) {
+      case 39:
+        let p = ballPosition["left"];
+        p = parseInt(p) + 5;
+        setBallPosition({ left: p });
+        break;
+      case 38:
+        let q = ballPosition["left"];
+        q = parseInt(q) + 5;
+        setBallPosition({ top: q });
+        break;
+      case 40:
+        let r = x;
+        r = parseInt(r) + 5;
+        setX(r);
+        break;
+      case 37:
+        let s = y;
+        s = parseInt(s) + 5;
+        setY(s);
+        break;
+      default:
+        break;
+    }
   });
   const reset = () => {};
-  const renderChoice = () => {};
+  const renderChoice = (renderBall) => {
+    console.log(renderBall);
+    if (!renderBall) {
+      return (
+        <button className="start" onClick={() => setRenderBall(true)}>
+          Start
+        </button>
+      );
+    } else {
+      return (
+        <div
+          className="ball"
+          style={{
+            right: `${x.toString()}`,
+            left: ballPosition["left"],
+            top: ballPosition["top"],
+            bottom: `${y.toString()}`
+          }}
+        ></div>
+      );
+    }
+  };
 
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
         Reset
       </button>
-      {renderChoice()}
+      {renderChoice(renderBall)}
     </div>
   );
 };
